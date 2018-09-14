d92cd125799afc5958c71940aebf7da92aed0540
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-09-13 23:34:01 -0500

    test

--- example/send.py
+++ example/send.py
@@ -1,17 +1,18 @@
+# -*- coding:utf-8 -*-
 import RPi.GPIO as GPIO
 import serial
-import binascii
-import time
 
-def hex2Str(argv):
-    result = ''   
-    hLen = len(argv)
-    print hLen   
-    for i in xrange(hLen):   
-        hvol = ord(argv[i])   
-        hhex = '%02x'%hvol   
-        result += hhex+' '   
-    return result 
+EN_485 =  4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(EN_485,GPIO.OUT)
+GPIO.output(EN_485,GPIO.HIGH)
+
+ser = serial.Serial("/dev/ttyS0",19200)
+
+print t.portstr    
+strInput = raw_input('enter some words:')    
+n = t.write(strInput)    
+print n
 
 def str2Hex(str):
     str = str.replace(' ', '')
@@ -19,25 +20,8 @@ def str2Hex(str):
     int_values = [int(h, base=16) for h in hex_values]
     return int_values
 
-EN_485 =  4
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(EN_485,GPIO.OUT)
-GPIO.output(EN_485,GPIO.LOW)
-
-ser = serial.Serial("/dev/ttyS0",19200,timeout=1)    
-# print t.portstr    
-# strInput = raw_input('enter some words:')    
-# n = t.write(strInput)    
-# print n    
-# str = t.read(n)    
-# print str   
-
 while True:
-    str = ser.readall()
-    # str = raw_input('enter some words:')
-    if str:
-        str = hex2Str(str)
-        print 'get %s' % str
-        str = str2Hex(str)
-        print 'write %s' % str
-      
+    str = raw_input('enter some words:')
+    str = str2Hex(str)
+    res = ser.write(str)
+    print 'res %s' % res
