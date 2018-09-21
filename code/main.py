4eba69ab10300f0b0ee8a5368d51621c36c9eab7
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-09-20 23:19:53 -0500

    main

--- code/main.py
+++ code/main.py
@@ -4,6 +4,10 @@ from serial_up_worker import SerialUpWorker
 from serial_down_worker import SerialDownWorker
 from socket_worker import SocketWorker
 
-thread.start_new_thread(SerialUpWorker, ())
-thread.start_new_thread(SerialDownWorker, ())
-thread.start_new_thread(SocketWorker, ())
+thread1 = thread.start_new_thread(SerialUpWorker, ())
+thread2 = thread.start_new_thread(SerialDownWorker, ())
+thread3 = thread.start_new_thread(SocketWorker, ())
+
+thread1.join()
+thread2.join()
+thread3.join()
