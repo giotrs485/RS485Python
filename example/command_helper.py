583d90febf62d15bc8f1ea06c300e5e0dc4dbf1d
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-10-04 22:58:05 -0500

    test

--- example/command_helper.py
+++ example/command_helper.py
@@ -14,5 +14,8 @@ class CommandHelper(object):
     @staticmethod
     def toReadable( command ):
         print len(command)
+        for i in range(len(command))
+            print command[i]
+            print ord(command[i])
         # command = bytes.fromhex(command)
         return command
