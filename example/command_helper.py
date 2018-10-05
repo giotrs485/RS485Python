ff8955356c2c303380eb2d74d0419f842e2c97e6
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-10-04 23:03:24 -0500

    test

--- example/command_helper.py
+++ example/command_helper.py
@@ -16,6 +16,6 @@ class CommandHelper(object):
         print len(command)
         for i in range(len(command)):
             print command[i]
-            print int(command[i], 32)
+            print int(command[i], 16)
         # command = bytes.fromhex(command)
         return command
