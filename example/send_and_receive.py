9b3441976b230278ccb06733e039110f0ad392fd
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-10-10 06:32:44 -0500

    client test

--- example/send_and_receive.py
+++ example/send_and_receive.py
@@ -5,23 +5,29 @@ import time
 
 from command_helper import CommandHelper
 
-test_command = '00'
+test_command = '0104000100162004'
+# test_command = '01'
 
 EN_485 = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(EN_485,GPIO.OUT)
 GPIO.output(EN_485,GPIO.HIGH)
 
-port = serial.Serial("/dev/ttyS0", 115200, timeout=1)
+port = serial.Serial("/dev/ttyS0", 115200, serial.EIGHTBITS, serial.PARITY_NONE, 999)
 
 while True:
     GPIO.output(EN_485,GPIO.HIGH)
     print 'send %s' % test_command
-    port.write( CommandHelper.toWriteable( test_command ) )
+    command = CommandHelper.toWriteable( test_command )
+	
+    # for i in range( len(command) ):	
+    #    port.write( command[i] )
+    #   time.sleep(1)
+    port.write(command)
 
     GPIO.output(EN_485,GPIO.LOW)
     result = port.readall()
     if result:
         result = CommandHelper.toReadable(result)
         print 'receive %s' % result
-    time.sleep(1)
+    # time.sleep(5)
