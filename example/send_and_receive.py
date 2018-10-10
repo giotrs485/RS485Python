eb995d6b03d5dfaed2217b310c904b68e4698bff
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-10-10 06:38:44 -0500

    test

--- example/send_and_receive.py
+++ example/send_and_receive.py
@@ -12,15 +12,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(EN_485,GPIO.OUT)
 GPIO.output(EN_485,GPIO.HIGH)
 
-port = serial.Serial("/dev/ttyS0", 115200)
+port = serial.Serial("/dev/ttyS0", 115200, timeout=1)
 
 while True:
     GPIO.output(EN_485,GPIO.HIGH)
+
     print 'send %s' % test_command
     command = CommandHelper.toWriteable( test_command )
 	
     # for i in range( len(command) ):	
-    #    port.write( command[i] )
+    #   port.write( command[i] )
     #   time.sleep(1)
     port.write(command)
 
@@ -29,5 +30,5 @@ while True:
     if result:
         result = CommandHelper.toReadable(result)
         print 'receive %s' % result
-        
+
     time.sleep(1)
