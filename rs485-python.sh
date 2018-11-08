dac1a9e92e789bbf4078a82a04668e6253a3d577
JarvisZhangUTA <zhangjw.uta@gmail.com>
2018-11-07 21:14:04 -0600

    test

--- rs485-python.sh
+++ rs485-python.sh
@@ -10,9 +10,9 @@ done
 
 echo 'config set stop-writes-on-bgsave-error no' | redis-cli
 
-ps aux | grep -ie nohup | awk '{print $2}' | sudo xargs kill -9
+ps aux | grep -ie nohup | awk '{print $2}' | xargs kill -9
 
-sudo nohup /usr/bin/python2.7 -u /home/lijian/Code/RS485Python/code/socket_worker.py > /home/lijian/Code/RS485Python/logs/socket_worker.log 2>&1 &
-sudo nohup /usr/bin/python2.7 -u /home/lijian/Code/RS485Python/code/serial_worker.py > /home/lijian/Code/RS485Python/logs/serial_worker.log 2>&1 &
+exec nohup /usr/bin/python2.7 -u /home/lijian/Code/RS485Python/code/socket_worker.py > /home/lijian/Code/RS485Python/logs/socket_worker.log 2>&1 &
+exec nohup /usr/bin/python2.7 -u /home/lijian/Code/RS485Python/code/serial_worker.py > /home/lijian/Code/RS485Python/logs/serial_worker.log 2>&1 &
 
 exit 0
